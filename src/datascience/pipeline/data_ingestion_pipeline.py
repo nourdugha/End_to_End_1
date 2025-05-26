94673e91e643723ba42bb2ef232605226bdb326d
nourdugha <nouraldeendugha@gmail.com>
2025-05-26 16:00:31 +0200

    fix_data_valid

--- src/datascience/pipeline/data_ingestion_pipeline.py
+++ src/datascience/pipeline/data_ingestion_pipeline.py
@@ -7,8 +7,11 @@ class DataIngestionTrainingPipeline:
     def __init__(self):
         pass
     def initiate_data_ingestion(self):
-        config=ConfigurationManager()
-        data_ingestion_config=config.get_data_ingestion_config()
-        data_ingestion=DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        try:
+            config=ConfigurationManager()
+            data_ingestion_config=config.get_data_ingestion_config()
+            data_ingestion=DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+        except Exception as e:
+            raise e
